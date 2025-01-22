3716af14d199b6359936060705d5feb27493d43a
Lori Rothermel <lori@rothermel1.com>
2025-01-21 21:22:39 -0800

    Initial Commit

---
+++ HP Trivia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HP Trivia
+//
+//  Created by Lori Rothermel on 1/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
